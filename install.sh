0d9382393c370f7ff86b544e7c21006251fd318d
arivarton <accounts@arivarton.com>
2016-12-15 23:13:45 +0000

    Added parameters

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 [ `whoami` = root ] || exec su -c $0 root
 
 USERNAME="vidr"
-PACKAGES="vim zsh htop python3"
+PACKAGES="vim zsh htop python3 python2 gnome"
 
 # Set up user and sudo rights
 useradd -m $USERNAME
@@ -32,10 +32,14 @@ if [ -e /bin/pacman ]; then
     mv -f /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist
   fi
   # Mirror ranking
-  cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-  sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-  echo "Ranking mirrors, this will take a while so relax and get a coffee."
-  rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+  read -p 'Rank mirrors? (y/N) ' -n 1 -r
+  while [[ $REPLY != ^(y|Y|n|N)$ ]]
+    do
+      cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+      sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+      echo "Ranking mirrors, this will take a while so relax and get a coffee."
+      rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+    done
 # Ubuntu/Debian
 elif [ -e /usr/bin/apt-get ]; then
   # Set package manager to apt-get
@@ -45,6 +49,22 @@ elif [ -e /usr/bin/apt-get ]; then
   apt-get update -y
   apt-get upgrade -y
 fi
+
+# Set time zone
+ln -s /usr/share/zoneinfo/Iceland /etc/localtime
+
+# Set locale
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+echo 'nb_no.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'is_IS.UTF-8 UTF-8' >> /etc/locale.gen
+
+# Set hostname
+read -p 'What should the hostname be? ' myhostname
+if [ -ne /etc/hostname ]; then
+  touch /etc/hostname
+fi
+echo myhostname > /etc/hostname
+echo '127.0.0.1       ' + myhostname + '.localdomain ' + myhostname >> /etc/hosts
 
 # Install all packages
 echo "Installing packages"
