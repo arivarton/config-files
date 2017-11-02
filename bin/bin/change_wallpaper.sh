271bad2adde4d415add6cfc23968a41e6cdc2f89
arivarton <accounts@arivarton.com>
2017-11-02 21:12:13 +0000

    Changed

--- bin/bin/change_wallpaper.sh
+++ bin/bin/change_wallpaper.sh
@@ -1,11 +1,8 @@
 #! /usr/bin/env bash
 
 STANDARD_WALLPAPER_PATH=$(cat .xinitrc | grep -E "STANDARD_WALLPAPER_PATH=.*$" | cut -d '=' -f 2 | sed "s|\~|$HOME|")
-new_wallpaper="${1/#\~/$HOME}"
 
 change() {
-    new_wallpaper="${1/#\~/$HOME}"
-    echo $new_wallpaper
     sed -i "s|WALLPAPER=.*|WALLPAPER=$1|" ~/.xinitrc
     convert $1 $STANDARD_WALLPAPER_PATH
     feh --bg-fill $(resize_wallpaper.sh)
